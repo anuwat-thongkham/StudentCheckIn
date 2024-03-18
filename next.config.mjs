43c0b7431a43dbb324cbb535e008b6b640fca1d1
“anuwat-thongkham” <anuwat.th@kkumail.com>
2024-03-18 23:35:57 +0700

    change framework cause by error

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: "/StudentCheckIn",
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-};
-
-export default nextConfig;
