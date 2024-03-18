8d6605fed8aa493ef25a8aff13955cbb61654ddb
“anuwat-thongkham” <anuwat.th@kkumail.com>
2024-03-18 18:04:22 +0700

    Update HomePage.tsx

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,7 +1,9 @@
 
 function HomePage() {
     return (
-        <div className="home-page"></div>
+        <div className="home-page">
+            <div className="home-page"></div>
+        </div>
     );
 }
 export default HomePage;
