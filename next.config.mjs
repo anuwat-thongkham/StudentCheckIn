a181679bb37add958c0201d6c801b1fc765167d7
“anuwat-thongkham” <anuwat.th@kkumail.com>
2024-03-18 22:12:29 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	output: 'export',
-};
+// next.config.mjs
 
-export default nextConfig;
+export default {
+    // Other Next.js configuration options...
+    // Your other configuration settings
+    
+    // Configure static HTML export
+    // You can customize these options based on your requirements
+    exportPathMap: async function () {
+      return {
+        '/': { page: '/' },
+        // Add other routes here if needed
+      };
+    },
+  };
+  
