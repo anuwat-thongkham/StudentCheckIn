3d2c878409705bbe3ee572490e7c49a7d55fd6cd
“anuwat-thongkham” <anuwat.th@kkumail.com>
2024-03-19 00:14:11 +0700

    Update ApplicationRouter.tsx

--- src/route/ApplicationRouter.tsx
+++ src/route/ApplicationRouter.tsx
@@ -6,7 +6,7 @@ const ApplicationRouter: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/StudentCheckIn" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
