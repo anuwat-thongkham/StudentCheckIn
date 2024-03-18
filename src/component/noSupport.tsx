426d3618b06f385a3acfcb7bdcc5e9c10d1c2d53
“anuwat-thongkham” <anuwat.th@kkumail.com>
2024-03-18 22:35:31 +0700

    Update noSupport.tsx

--- src/component/noSupport.tsx
+++ src/component/noSupport.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image'; 
 import '../style/global/global.scss';
 import '../style/conponent/noSupportStyle.scss';
 export default function NoSupport() {
     return (
         <div className="noSupport">
-            <Image className='noSupportImage' src="./asset/svg/noSupport.svg" alt="No Support" width="100" height="100"  priority/>
+            <img className='noSupportImage' src="./asset/svg/noSupport.svg" alt="No Support" width="100" height="100"  />
             <p className='noSupportDescription'>รองรับการแสดงผลเฉพาะบนสมาร์ทโฟน</p>
         </div>
     )
