d2deb2e679504b8681e66aad69ba841d587e5ae2
“anuwat-thongkham” <anuwat.th@kkumail.com>
2024-03-19 00:06:31 +0700

    fix error

--- src/component/NoSupport.tsx
+++ src/component/NoSupport.tsx
@@ -1,9 +1,10 @@
 import '../style/global/global.scss';
 import '../style/conponent/noSupportStyle.scss';
+import noSupportImage from '../assets/svg/noSupport.svg'
 export default function NoSupport() {
     return (
         <div className="noSupport">
-            <img className='noSupportImage' src="./asset/svg/noSupport.svg" alt="No Support" width="100" height="100"  />
+            <img className='noSupportImage' src={noSupportImage} alt="No Support" width="100" height="100"  />
             <p className='noSupportDescription'>รองรับการแสดงผลเฉพาะบนสมาร์ทโฟน</p>
         </div>
     )
--- src/route/ApplicationRouter.tsx
+++ src/route/ApplicationRouter.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from '../page/HomePage'
+
 const ApplicationRouter: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/StudentCheckIn" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
