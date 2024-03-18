e929fb1fc8916086555c8d592e5f41bae87e147a
“anuwat-thongkham” <anuwat.th@kkumail.com>
2024-03-18 18:09:10 +0700

    fix error

--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -3,7 +3,7 @@ function HomePage() {
     return (
         <div className="home-page">
             <div className="home-page">
-                <h1></h1>
+                <h1>hi</h1>
             </div>
         </div>
     );
