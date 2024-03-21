15b310b7ec5b293586638f20035a1b416c654748
“anuwat-thongkham” <anuwat.th@kkumail.com>
2024-03-21 16:33:02 +0700

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,22 +1,22 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyDr-7SK3FPB-cTapRcWrX3GhvZVqFLKacY",
-    authDomain: "studentchecksystem.firebaseapp.com",
-    projectId: "studentchecksystem",
-    storageBucket: "studentchecksystem.appspot.com",
-    messagingSenderId: "333681254194",
-    appId: "1:333681254194:web:4a0c48d1910caf136d812b",
-    measurementId: "G-9RLGKKNPXS"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDr-7SK3FPB-cTapRcWrX3GhvZVqFLKacY",
+//     authDomain: "studentchecksystem.firebaseapp.com",
+//     projectId: "studentchecksystem",
+//     storageBucket: "studentchecksystem.appspot.com",
+//     messagingSenderId: "333681254194",
+//     appId: "1:333681254194:web:4a0c48d1910caf136d812b",
+//     measurementId: "G-9RLGKKNPXS"
+// };
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+// const firebaseApp = initializeApp(firebaseConfig);
 // Initialize Firebase Auth provider
 const provider = new GoogleAuthProvider();
   
